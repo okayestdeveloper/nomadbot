838eddfa321f49567a3fac3fd9df38fd5a892d5a
Brad Ledbetter <brad.l@leantaas.com>
2020-04-01 08:17:38 -0400

    Refactor handler to accept a User as part of arguments and return a Promise

--- commands/help/index.js
+++ commands/help/index.js
@@ -1,46 +1,51 @@
 const fs = require('fs');
 const path = require('path');
+const { formatHelpText } = require('../../shared/formatters');
 
-function formatHelpText(command, helpText) {
-  return `- **!${command}**\n` +
-    `    ${helpText}\n`;
-}
-
+/**
+ * Return true if the given path is a directory
+ * @param {string} path
+ * @returns {boolean}
+ */
 function isDir(path) {
   const fd = fs.openSync(path, 'r', 0o666);
   const stat = fs.fstatSync(fd);
-  fs.close(fd, () => {});
+  fs.close(fd, () => { });
   return stat.isDirectory();
 }
 
-function handler({ message, logger, username }) {
-  const commandDir = path.resolve(__dirname, '..');
-  const files = fs.readdirSync(commandDir);
+/**
+ * Take a list of files and concatenate their contents into a formatted string
+ * @param {string} msg accumlator for the output string
+ * @param {string} command the command that was given, aka the folder where the help text is
+ * @returns {string}
+ */
+const filesReducer = (msg, command) => {
+  if (command !== '.' && command !== '..') {
+    const dirPath = path.resolve(commandDir, command);
 
-  const msgText = `\n` + files.reduce((msg, command) => {
-    if (command !== '.' && command !== '..') {
-      const dirPath = path.resolve(commandDir, command);
-
-      if (isDir(dirPath)) {
-        try {
-          const helpText = fs.readFileSync(path.resolve(dirPath, 'help.md'));
-          msg += formatHelpText(command, helpText);
-        } catch (ex) {
-          if (ex.code !== 'ENOENT') {
-            logger.info(`Help command caught exception ${ex}`);
-          }
+    if (isDir(dirPath)) {
+      try {
+        const helpText = fs.readFileSync(path.resolve(dirPath, 'help.md'));
+        msg += formatHelpText(command, helpText);
+      } catch (ex) {
+        if (ex.code !== 'ENOENT') {
+          logger.info(`Help command caught exception ${ex}`);
         }
       }
     }
+  }
 
-    return msg;
-  }, []);
+  return msg;
+}
 
+function handler({ message }) {
+  const commandDir = path.resolve(__dirname, '..');
+  const files = fs.readdirSync(commandDir);
+  const msgText = `\n` + files.reduce(filesReducer, []);
 
   if (msgText && msgText.length >= 0) {
-    message.reply(msgText)
-      .then(() => logger.info(`Nomadbot replied to ${message.content} from ${username}`))
-      .catch((err) => logger.error(err));
+    return message.reply(msgText);
   }
 }
 
