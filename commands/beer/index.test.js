c228047ab3bdefdb5d47a1f4ba2ffc6a876b0ab6
Brad Ledbetter <brad.l@leantaas.com>
2020-04-16 07:50:47 -0400

    Add tests for beer command

--- commands/beer/index.test.js
+++ commands/beer/index.test.js
@@ -1,5 +1,33 @@
+const MockMessage = require('../../shared/message.mock');
+
+require('../../shared/firebase');
+jest.mock('../../shared/firebase');
+
+const formatters = require('../../shared/formatters');
+jest.mock('../../shared/formatters');
+
+const handler = require('./index.js');
+
 describe('!beer', () => {
-  it('should fail', () => {
-    expect(false).toEqual(true);
+  let message;
+
+  beforeEach(() => {
+    message = new MockMessage();
   });
+
+  it('should format a beer', () => {
+    return handler({ message, logger: console })
+      .then(() => {
+        expect(formatters.formatBeer.mock.calls.length).toEqual(1);
+      });
+  });
+
+  it('should resolve to a beer', () => {
+    return handler({ message, logger: console })
+      .then((beer) => {
+        const matches = beer.match(/(beer)/g);
+        expect(matches.length).toEqual(1)
+      });
+  });
+
 });
