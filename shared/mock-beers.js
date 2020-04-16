5502d072222c7a59448b1367d752f6e2575d868f
Brad Ledbetter <brad.l@leantaas.com>
2020-04-16 07:50:08 -0400

    Refactor mock beer data to better mimic firebase.database.DataSnapshot

--- shared/mock-beers.js
+++ shared/mock-beers.js
@@ -1,20 +1,51 @@
-module.exports = {
-  mockBeers: [
-    {
+const mockBeers = [
+  {
+    ontap: true,
+    name: 'beer 1',
+    data: () => ({
       ontap: true,
       name: 'beer 1',
-      data: () => ({
-        ontap: true,
-        name: 'beer 1',
-      }),
-    },
-    {
+    }),
+  },
+  {
+    ontap: false,
+    name: 'beer 2',
+    data: () => ({
       ontap: false,
       name: 'beer 2',
-      data: () => ({
-        ontap: false,
-        name: 'beer 2',
-      }),
-    },
-  ]
+    }),
+  },
+];
+
+class MockSnapshot {
+  constructor(data) {
+    this.docs = data;
+  }
+
+  get length() {
+    return this.docs.length;
+  }
+
+  get size() {
+    return this.docs.length;
+  }
+
+  filter(cb) {
+    this.docs = this.docs.filter(cb);
+    return this;
+  }
+
+  forEach(cb) {
+    this.docs = this.docs.filter(cb);
+    return this;
+  }
+
+  map(cb) {
+    this.docs = this.docs.filter(cb);
+    return this;
+  }
+}
+
+module.exports = {
+  mockBeers: new MockSnapshot(mockBeers),
 };
